1d02089af096ef971c2a89bac8f52f104349ba93
Amorem <dev@amorem.com>
2020-03-28 19:34:17 +0100

    Fix. And remove update graphql call

--- src/Routes/Auth/AuthContainer.js
+++ src/Routes/Auth/AuthContainer.js
@@ -11,19 +11,9 @@ export default () => {
   const firstName = useInput("");
   const lastName = useInput("");
   const email = useInput("");
-  const [requestSecret] = useMutation(LOG_IN, {
-    update: (_, { data }) => {
-      const { reqSec } = data;
-      if (!reqSec) {
-        toast.error("You dont have an account yet, create one !");
-        setTimeout(() => setAction("signUp"), 3000);
-      }
-    },
+  const [requestSecretMutation] = useMutation(LOG_IN, {
     variables: {
-      email: email.value,
-      username: username.value,
-      firstName: firstName.value,
-      lastName: lastName.value
+      email: email.value
     }
   });
 
@@ -36,18 +26,32 @@ export default () => {
     }
   });
 
-  const onSubmit = e => {
+  const onSubmit = async e => {
     e.preventDefault();
 
     if (action === "logIn") {
       if (email.value !== "") {
-        requestSecret();
+        try {
+          const data = await requestSecretMutation();
+          console.log(data);
+          const { requestSecret } = data;
+          if (!requestSecret) {
+            toast.error("You dont have an account yet, create one !");
+            setTimeout(() => setAction("signUp"), 3000);
+          }
+        } catch {
+          toast.error("Cannot complete action. Please try again later");
+        }
       } else {
         toast.error("Email id required");
       }
     } else if (action === "SignUp") {
       if (email.value !== "" && username.value !== "") {
-        createAccount();
+        try {
+          await createAccount();
+        } catch {
+          toast.error("Cannot complete action. Please try again later");
+        }
       } else {
         toast.error("Email & Username are required fields");
       }
@@ -62,7 +66,7 @@ export default () => {
       firstName={firstName}
       lastName={lastName}
       email={email}
-      onLogin={onSubmit}
+      onSubmit={onSubmit}
     />
   );
 };
