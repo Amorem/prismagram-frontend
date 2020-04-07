9910cc90340eefba5ded352acce0c82f2b9c8184
Amorem <dev@amorem.com>
2020-04-07 08:13:54 +0200

    SearchPresenter

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -2,17 +2,56 @@ import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 import FatText from "../../Components/FatText";
+import Loader from "../../Components/Loader";
+import UserCard from "../../Components/UserCard";
 
 const Wrapper = styled.div`
   height: 50vh;
-  text-align: center;
 `;
 
-const SearchPresenter = ({ searchTerm, loading }) => (
-  <Wrapper>
-    {searchTerm === undefined && <FatText text="Search for something!" />}
-  </Wrapper>
-);
+const Section = styled.div``;
+
+const SearchPresenter = ({ searchTerm, loading, data }) => {
+  if (searchTerm === undefined) {
+    return (
+      <Wrapper>
+        <FatText text={"Search for something"} />
+      </Wrapper>
+    );
+  } else if (loading === true) {
+    return (
+      <Wrapper>
+        <Loader />
+      </Wrapper>
+    );
+  } else if (data && data.searchPost & data.searchUser) {
+    return (
+      <Wrapper>
+        <Section>
+          {data.searchUser.length === 0 ? (
+            <FatText text="No user found" />
+          ) : (
+            data.searchUser.map((user) => (
+              <UserCard
+                username={user.username}
+                isFollowing={user.isFollowing}
+                url={user.url}
+                isSelf={user.isSelf}
+              />
+            ))
+          )}
+        </Section>
+        <Section>
+          {data.searchPost.length === 0 ? (
+            <FatText text="No photos found" />
+          ) : (
+            data.searchPost.map((post) => null)
+          )}
+        </Section>
+      </Wrapper>
+    );
+  }
+};
 
 SearchPresenter.propTypes = {
   searchTerm: PropTypes.string,
