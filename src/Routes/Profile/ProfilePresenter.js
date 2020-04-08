431a6cfebe42773b9f8bd428839523ee76433434
Amorem <dev@amorem.com>
2020-04-08 17:19:16 +0200

    Profile is OK

--- src/Routes/Profile/ProfilePresenter.js
+++ src/Routes/Profile/ProfilePresenter.js
@@ -1,15 +1,64 @@
 import React from "react";
 import styled from "styled-components";
+import { Helmet } from "react-helmet";
 import Loader from "../../Components/Loader";
 import Avatar from "../../Components/Avatar";
+import FatText from "../../Components/FatText";
+import FollowButton from "../../Components/FollowButton";
+import SquarePost from "../../Components/SquarePost";
 
 const Wrapper = styled.div`
-  min-height: 60vh;
+  min-height: 80vh;
 `;
 
-const Header = styled.header``;
+const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 80%;
+  margin: 0 auto;
+  margin-bottom: 40px;
+`;
+
+const Posts = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 200px);
+  grid-template-rows: 200px;
+  grid-auto-rows: 200px;
+`;
 
 const HeaderColumn = styled.div``;
+
+const UsernameRow = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Username = styled.span`
+  font-size: 26px;
+  margin-bottom: 10px;
+  display: block;
+`;
+
+const Counts = styled.ul`
+  display: flex;
+  margin: 15px 0px;
+`;
+
+const Count = styled.li`
+  font-size: 16px;
+  &:not(:last-child) {
+    margin-right: 10px;
+  }
+`;
+
+const FullName = styled(FatText)`
+  font-size: 16px;
+`;
+
+const Bio = styled.p`
+  margin: 10px 0px;
+`;
 
 export default ({ loading, data }) => {
   if (loading) {
@@ -21,6 +70,7 @@ export default ({ loading, data }) => {
   } else {
     const {
       seeUser: {
+        id,
         avatar,
         username,
         fullName,
@@ -33,13 +83,49 @@ export default ({ loading, data }) => {
         postsCount,
       },
     } = data;
-    console.log(data);
+
     return (
-      <Header>
-        <HeaderColumn>
-          <Avatar size="lg" url={avatar} />
-        </HeaderColumn>
-      </Header>
+      <Wrapper>
+        <Helmet>
+          <title>{username} | Prismagram</title>
+        </Helmet>
+        <Header>
+          <HeaderColumn>
+            <Avatar size="lg" url={avatar} />
+          </HeaderColumn>
+          <HeaderColumn>
+            <Username>{username}</Username>
+            <UsernameRow>
+              {!isSelf && <FollowButton id={id} isFollowing={isFollowing} />}
+            </UsernameRow>
+
+            <Counts>
+              <Count>
+                <FatText text={String(postsCount)} /> posts
+              </Count>
+              <Count>
+                <FatText text={String(followersCount)} /> followers
+              </Count>
+              <Count>
+                <FatText text={String(followingCount)} /> following
+              </Count>
+            </Counts>
+            <FullName text={fullName} />
+            <Bio>{bio}</Bio>
+          </HeaderColumn>
+        </Header>
+        <Posts>
+          {posts &&
+            posts.map((post) => (
+              <SquarePost
+                key={post.id}
+                likeCount={post.likeCount}
+                commentCount={post.commentCount}
+                file={post.files[0]}
+              />
+            ))}
+        </Posts>
+      </Wrapper>
     );
   }
 };
